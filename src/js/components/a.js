916d04d67374bd00c33ed9d36fc2db30f573e9d9
группы стационара <st@itstep.lan>
2021-02-08 21:44:23 +0300

    Added webpack config

---
+++ src/js/components/a.js
@@ -0,0 +1,10 @@
+
+import * as _ from 'lodash';
+
+var array = [1, [2, [3, [4]], 5]];
+ 
+_.flattenDepth(array, 1);
+// => [1, 2, [3, [4]], 5]
+ 
+_.flattenDepth(array, 2);
+// => [1, 2, 3, [4], 5]
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import './components/a';
+import './components/b';
+
